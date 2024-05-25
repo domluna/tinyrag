ac8650d46cba69a377396f7eff6a9ae0ee3ad366
Dominique Luna <dluna132@gmail.com>
2024-05-24 20:20:54 -0400

    works well

--- hnsw.jl
+++ hnsw.jl
@@ -1,21 +1,43 @@
+using StaticArrays
+
 include("heap.jl")
 
+
 mutable struct HNSW
+    # set this higher based on the dimensionality of the data
+    # the paper suggests a range from 5 to 48
     const connectivity::Int
+    # the number of connections to keep at the zeroth layer
+    # the paper suggests this to be twice the connectivity
+    # lower and the search is not as good, higher and the search is too slow
+    const connectivity0::Int
+    # optimal value seems to be 1 / log(connectivity)
     const mL::Float64
 
     const graphs::Dict{Int,Dict{Int,Vector{Int}}}
 
     enter_point::Int
-    data::Vector{Int}
+    data::Vector{SVector{8,UInt64}}
 
-    function HNSW(; connectivity = 16, mL = 1 / log(connectivity))
-        new(connectivity, mL, Dict{Int,Dict{Int,Vector{Int}}}(), 1, Int[])
+    function HNSW(; connectivity::Int=16, connectivity0::Int=connectivity * 2, mL::Float64=1 / log(connectivity))
+        v = Vector{SVector{8,UInt64}}[]
+        new(connectivity, connectivity0, mL, Dict{Int,Dict{Int,Vector{Int}}}(), 1, v)
     end
 end
 
-function _distance(x::Int, y::Int)::Int
-    return abs(x - y)
+@inline function hamming_distance(x1::T, x2::T)::Int where {T<:Integer}
+    return Int(count_ones(x1 ⊻ x2))
+end
+
+@inline function hamming_distance(
+    x1::AbstractArray{T},
+    x2::AbstractArray{T},
+)::Int where {T<:Integer}
+    s = 0
+    @inbounds @simd for i in eachindex(x1, x2)
+        s += hamming_distance(x1[i], x2[i])
+    end
+    s
 end
 
 function _get_level(hnsw::HNSW)::Int
@@ -24,17 +46,18 @@ end
 
 function _search_layer(
     hnsw::HNSW,
-    query::Int,
+    query::SVector{8,UInt64},
     ep::Int,
     expansion_factor::Int,
     level::Int;
-    k::Int = 0,
+    k::Int=0, # used for search we only need to keep the top k candidates
 )::Vector{Int}
     visited = Set{Int}([ep])
+    # candidates = MinHeap(k > 0 ? k : expansion_factor)
     candidates = MinHeap(expansion_factor)
     W = MaxHeap(k > 0 ? k : expansion_factor)
 
-    d = _distance(query, hnsw.data[ep])
+    d = hamming_distance(query, hnsw.data[ep])
     insert!(candidates, d => ep)
     insert!(W, d => ep)
 
@@ -42,6 +65,7 @@ function _search_layer(
         d_c, c = pop!(candidates)
         d_f = W[1].first
 
+        # no need to search further
         if d_c > d_f
             break
         end
@@ -51,27 +75,24 @@ function _search_layer(
                 continue
             end
             push!(visited, e)
-            d_e = _distance(query, hnsw.data[e])
-            d_f = W[1].first
 
-            if d_e < d_f || length(W) < expansion_factor
+            d_e = hamming_distance(query, hnsw.data[e])
+            d_f = W[1].first
+            if d_e < d_f || length(W) < W.k
                 # insert! will automatically remove the largest element if the heap is full
                 insert!(W, d_e => e)
                 insert!(candidates, d_e => e)
             end
         end
     end
-
-    mx = hnsw.connectivity
-    k = min(length(W), mx)
-    sort!(W.data, by = x -> x[1])
-    return Int[W.data[i][2] for i = 1:k]
+    # @info "" level length(visited)
+    sort!(W.data, by=x -> x[1])
+    return Int[W.data[i][2] for i = 1:length(W)]
 end
 
-function Base.insert!(hnsw::HNSW, q::Int; expansion_factor::Int = 128)
+function Base.insert!(hnsw::HNSW, q::SVector{8,UInt64}; expansion_factor::Int=100)
     push!(hnsw.data, q)
     ind = length(hnsw.data)
-    ep = hnsw.enter_point
     l = _get_level(hnsw)
     new_entry_point = l > length(hnsw.graphs)
 
@@ -83,23 +104,28 @@ function Base.insert!(hnsw::HNSW, q::Int; expansion_factor::Int = 128)
             end
         end
     end
+
+    if ind == 1
+        for level = l:-1:1
+            hnsw.graphs[level][ind] = Int[]
+        end
+        return
+    end
     L = length(hnsw.graphs)
 
+    ep = hnsw.enter_point
     for level = L:-1:l+1
         W = _search_layer(hnsw, q, ep, 1, level)
         ep = W[1]
     end
 
     for level = l:-1:1
+        mx = level == 1 ? hnsw.connectivity0 : hnsw.connectivity
         neighbors = _search_layer(hnsw, q, ep, expansion_factor, level)
+        neighbors = neighbors[1:min(hnsw.connectivity, length(neighbors))]
         # bi-directional connection
-        # hnsw.graphs[level][ind] = neighbors
-        hnsw.graphs[level][ind] = Int[]
+        hnsw.graphs[level][ind] = neighbors
         for n in neighbors
-            if n == ind
-                continue
-            end
-            push!(hnsw.graphs[level][ind], n)
             if !haskey(hnsw.graphs[level], n)
                 hnsw.graphs[level][n] = Int[ind]
             else
@@ -110,12 +136,11 @@ function Base.insert!(hnsw::HNSW, q::Int; expansion_factor::Int = 128)
         end
 
         # shrink the neighbors if necessary
-        mx = hnsw.connectivity
         for n in neighbors
-            if length(hnsw.graphs[level][n]) > 2 * mx
+            if length(hnsw.graphs[level][n]) > mx
                 hnsw.graphs[level][n] = sort!(
                     hnsw.graphs[level][n],
-                    by = x -> _distance(hnsw.data[x], hnsw.data[n]),
+                    by=x -> hamming_distance(hnsw.data[x], hnsw.data[n]),
                 )[1:mx]
             end
         end
@@ -128,52 +153,43 @@ function Base.insert!(hnsw::HNSW, q::Int; expansion_factor::Int = 128)
     return
 end
 
-function search(hnsw::HNSW, query::Int, k::Int; expansion_search::Int = 64)
-    ep = hnsw.enter_point
-    # @info "Searching for $k neighbors" query ep
+function search(hnsw::HNSW, query::SVector{8,UInt64}, k::Int; expansion_search::Int=50)
     L = length(hnsw.graphs)
+    ep = hnsw.enter_point
     for level = L:-1:2
-        # @time W = _search_layer(hnsw, query, ep, 1, level)
-        W = _search_layer(hnsw, query, ep, 1, level)
-        ep = W[1]
+        ep = _search_layer(hnsw, query, ep, 1, level)[1]
     end
-    # @time W = _search_layer(hnsw, query, ep, expansion_search, 1; k=k)
-    W = _search_layer(hnsw, query, ep, expansion_search, 1; k = k)
-    # return W
-    inds = [W[i] for i = 1:k]
-    vals = [hnsw.data[i] for i in inds]
-    return (inds, vals)
+    W = _search_layer(hnsw, query, ep, expansion_search, 1; k=k)
+    return W
 end
 
-function run0(n::Int)::HNSW
-    hnsw = HNSW(; connectivity = 16)
-    rng = 1:1_000_000
+function construct(n::Int; connectivity::Int=16)::HNSW
+    hnsw = HNSW(; connectivity=connectivity, connectivity0=connectivity * 2)
     for _ = 1:n
-        insert!(hnsw, rand(rng))
+        q = SVector{8,UInt64}(rand(UInt64, 8))
+        insert!(hnsw, q)
     end
-    search0(rand(rng), hnsw, 5)
     return hnsw
 end
 
-function search0(query::Int, hnsw::HNSW, k::Int)
-    (inds, vals) = search(hnsw, query, k)
-    distances = [_distance(query, data_point) for data_point in hnsw.data]
+function search0(hnsw, query::SVector{8,UInt64}, k::Int)
+    inds = search(hnsw, query, k)
+    approx_dists = [hamming_distance(query, hnsw.data[i]) for i in inds]
+    distances = [hamming_distance(query, hnsw.data[i]) for i in 1:length(hnsw.data)]
     k_nearest_manual = sortperm(distances)[1:k]
 
-    @info "Searching for $k neighbors" query
     println("HNSW inds ", sort(inds))
     println("Manual inds ", sort(k_nearest_manual))
-    println("HNSW vals ", sort(vals))
-    println("Manual vals ", sort([hnsw.data[i] for i in k_nearest_manual]))
+    println("HNSW distances ", sort(approx_dists))
+    println("Manual distances ", sort([hamming_distance(hnsw.data[i], query) for i in k_nearest_manual]))
 
     # print length of each graph layer
     for lc = 1:length(hnsw.graphs)
         println("Layer $lc: length = $(length(hnsw.graphs[lc]))")
     end
-    return
 end
 
 function search0(hnsw::HNSW, k::Int)
-    q = rand(1:1_000_000)
-    search0(q, hnsw, k)
+    q = SVector{8,UInt64}(rand(UInt64, 8))
+    search0(hnsw, q, k)
 end
